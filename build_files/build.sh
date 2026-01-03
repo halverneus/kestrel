e59ff8b1ecd1a58b1b071722fae6754621f5576e
Jeromy Streets <halverneus@gmail.com>
2026-01-03 15:16:06 -0800

    Added OpenRGB for keyboard colors.

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,4 +24,4 @@ set -ouex pipefail
 # systemctl enable podman.socket
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
-dnf5 -y install steam gamescope simple-scan
+dnf5 -y install steam gamescope simple-scan openrgb
